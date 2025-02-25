af6d44e3e0de7b171fa0d89b175786a8ef71c9da
Ana Barbiero <anacarolinabarbiero@gmail.com>
2025-02-25 10:12:45 -0400

    update

--- lembrete-agendamento.py
+++ lembrete-agendamento.py
@@ -9,8 +9,35 @@ import email
 import schedule
 import logging
 from email.message import EmailMessage
+from dotenv import load_dotenv
+import os
 
-def ler_agendamentos(): 
+load_dotenv()
+
+email_gerenciador = os.getenv('EMAIL_REMETENTE')
+password = os.getenv('PASSWORD')
+
+
+
+def salvar_dados():
+    with open('emails_paciente_lembrete', 'w', encoding= 'utf-8') as arquivo:
+        
+
+
+def ler_agendamentos(destinatario, mensagem): 
     with open('agenda.json', 'r', encoding= 'utf-8') as arquivo:
         dados = json.load(arquivo)
-    
+    email = dados.get('email')
+    hora = dados.get('hora')
+    data = dados.get('dia')
+
+
+def enviar_lembrete():
+    msg = EmailMessage()
+    msg['Subject'] = 'Lembrete de consulta agendada'
+    msg['From'] = email_gerenciador
+    msg['To'] = destinatario
+    msg.set_content(mensagem)
+
+
+# para nao esquecer - buscar o nome do paciente da lista json de pacientes - porém, buscar o horario =, email e data a ser enviado da lista de agendamentos json - Agr, pensar se é melhor ou nao criar um arquivo json com os emails e tudo que deve ser enviado do mesmo e deixar salvos
